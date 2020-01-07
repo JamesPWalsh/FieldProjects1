cfca592ee6994bb4c6cecf27edd798711e30a988
JamesPWalsh <33224799+JamesPWalsh@users.noreply.github.com>
2020-01-07 12:27:42 -0600

    Create playerpage.php
    changed html to php

---
+++ playerpage.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html>
+<!-- James Walsh 11/26/2019 -->
+<!--Last Updated: 01/06/2020-->
+<head>
+<link rel="stylesheet" href="css/playerstyle.css">
+<title>IHCC ESports - Player Info</title>
+</head>
+
+<body>
+	<div class="col-2">
+		<header>
+			<div id = "nav-bar">
+				<img class = "team-logo" src="#" alt = "team-logo"/>
+				<ul id="nav-bar-links">
+					<li><a href="#">Blog</a></li>
+					<li><a href="#">Events</a></li>
+					<li><a href="#">Teams</a></li>
+					<li><a href="#">About</a></li>
+					<li><a href="#">Home</a></li>
+				</ul>
+			</div><!--End of nav-bar-->
+		</header>
+
+		<div class="row">
+			<div class="column left">
+				<img src="images/img_player.jpg" alt="Profile Picture" class="player-img">
+				<div class="info">
+					<h4>Username:</h4><label for="male">Username</label><br>
+					<h4>E-Mail:</h4><label for="male">player@stu.indianhills.edu</label><br>
+					<h4>Phone #:</h4><label for="male">555-8001</label><br>
+					<h4>Favorite Game:</h4><label for="male">Frogger</label><br>
+					<h4>Schedule</h4>
+				</div>
+			</div>
+			<div class="column right">
+				<div class="strokeme">
+				<h2>Player Name</h2>
+				<h3>Bio: lorumipsum stuff and stuff and stuff and stuff and stuff and stuff and stuff and stuff and stuff and stuff and stuff</h3>
+				<h3>Game Stats</h3>
+				<h3>Player Ranking</h3>
+				</div>
+			</div>
+		</div>
+
+		<footer>Footer - eSports Links and Info</footer>
+	</div>
+</body>
+
+</html>
