07f7b7c502d27d908d1828fdc40c4204a365d020
JamesPWalsh <33224799+JamesPWalsh@users.noreply.github.com>
2020-01-16 12:52:14 -0600

    formating

---
+++ leaderpage.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<!-- James Walsh 01/16/2020 -->
+<!--Last Updated: 01/16/2020-->
+<head>
+<link rel="stylesheet" href="css/leaderstyle.css">
+<title>IHCC ESports - Leader Page</title>
+</head>
+
+<body>
+	<div class="col-2">
+		<header>
+			<div id = "nav-bar">
+				<img class = "team-logo" src="#" alt = "team-logo"/>
+				<ul id="nav-bar-links">
+					<li><a href="#">Blog</a></li>
+					<li><a href="#">Events</a></li>
+					<li><a href="team-page.html">Teams</a></li>
+					<li><a href="#">About</a></li>
+					<li><a href="index.html">Home</a></li>
+				</ul>
+			</div><!--End of nav-bar-->
+		</header>
+		<footer>Footer - eSports Links and Info</footer>
+	</div>
+</body>
+
+</html>
