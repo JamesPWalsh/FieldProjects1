9f83d820da209e43c928dcfc6a4a927ef5ffca2a
JamesPWalsh <33224799+JamesPWalsh@users.noreply.github.com>
2020-01-23 13:38:06 -0600

    leaderpage-update-1/23

--- leaderpage.php
+++ leaderpage.php
@@ -16,25 +16,25 @@
 					<li><a href="#">Blog</a></li>
 					<li><a href="#">Events</a></li>
 					<li><a href="team-page.html">Teams</a></li>
-					<li><a href="#">About</a></li>
+					<li><a href="aboutpage">About</a></li>
 					<li><a href="index.html">Home</a></li>
 				</ul>
 			</div><!--End of nav-bar-->
 		</header>
 		
 		<div class="col-container">
-			<div class="col-left">
-				<h2>Player Invites</h2>
-				<p>Choose Team: Invite Link</p>
-				<button class="invitebtn">Generate Invite Link</button>
-				<input type="text" name="invitecode" value="" readonly>
+			<div class="col-left"  style="border-right: 2px solid black;">
+				<h2>Edit Games</h2>
+				<select>
+					<option value="overwatch">Overwatch</option>
+					<option value="rocketleague">Rocket League</option>
+					<option value="csgo">CS:GO</option>
+					<option value="smashbro">Smash Bros.</option>
+				</select>
+				<button class="editgamebtn">Edit Page</button>
 			</div>
-			<div class="col-middle">
-				<h2>Edit Info</h2>
-				<p>Choose Game/Player: Edit Info</p>
-			</div>
-			<div class="col-right">
-				<h2>Add/Delete Pages</h2>
+			<div class="col-middle" style="border-right: 2px solid black;">
+				<h2>Delete Pages</h2>
 				<p>Delete Player/Game</p>
 				<select>
 					<option value="overwatch">Overwatch</option>
@@ -43,10 +43,36 @@
 					<option value="smashbro">Smash Bros.</option>
 					<option value="player">Player</option>
 				</select>
+				<button class="deletebtn">Delete Page</button>
 				<br>
-				<p>Add Game</p>
-				
+			</div>			
+			
+			<div class="col-right">
+				<h2>Player Invites</h2>
+				<p>Choose Team: Invite Link</p>
+				<button class="invitebtn">Generate Invite Link</button>
+				<input style="width: 80px;" type="text" name="invitecode" value="" readonly>
 			</div>
+		</div>
+		
+		<div class="col-container">
+			<div class="col-left"  style="border-right: 2px solid black;  border-top: 2px solid black;">
+				<h2>Edit Players</h2>
+				<select>
+					<option value="player">Player</option>
+				</select>
+				<button class="editplayerbtn">Edit Page</button>
+			</div>
+
+			<div class="col-middle"  style="border-right: 2px solid black; border-top: 2px solid black;">
+				<h2>Add Pages</h2>
+				<p>Add Game</p>
+				<input type="text" name="newgamename" value="">
+				<button class="addgamebtn">Add New Game</button>
+			</div>
+
+			<div class="col-right">
+			</div>			
 		</div>
 		
 		<footer>Footer - eSports Links and Info</footer>
