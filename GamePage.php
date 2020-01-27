9bcb7e1278d0ae4ac53cb6f345e2000e4a1d4c2d
kelseycrosby <44840736+kelseycrosby@users.noreply.github.com>
2020-01-27 12:21:58 -0600

    PHP Added to Pages
    PHP added to team page

--- GamePage.php
+++ GamePage.php
@@ -4,7 +4,6 @@
 	if(isset($_GET['name']))
 	{
 		$search = htmlspecialchars($_GET["name"]);
-		
 		include "php/database.php";
 		// Create connection
 		$conn = new mysqli($servername, $username, $password, $dbname);
@@ -15,61 +14,55 @@
 
 		$sql = "SELECT * FROM players WHERE game_name = '".$search."'";
 		$result = $conn->query($sql);
+	} else {
+		include "php/database.php";
+		// Create connection
+		$conn = new mysqli($servername, $username, $password, $dbname);
+		// Check connection
+		if ($conn->connect_error) {
+			die("Connection failed: " . $conn->connect_error);
+		}
+		$sql = "SELECT game_name FROM games";
+		$result = $conn->query($sql);
+		$row = $result->fetch_assoc();
+		$search = $row["game_name"];
+
+		$sql = "SELECT * FROM players WHERE game_name = '".$search."'";
+		$result = $conn->query($sql);
 	}
 	?>
  <head>
 	 <title>Game Page</title>
 	 <meta name="viewport" content="width=device-width, initial-
 	 scale=1">
-	 <link rel="stylesheet" href="css/bootstrap.min.css" />
+	 <link rel="stylesheet" href="css/navbar.css" />
 	 <link rel="stylesheet" href="css/gamepagestyle.css" />
  </head>
  
  <body>
-	<nav class="navbar navbar-expand-md navbar-dark bg-dark justify-content-center">
-	
-		<div class="container">
-			<a  class="navbar-brand"><h2>Game Page</h2></a>
-
-			<button class="navbar-toggler" data-toggle="collapse" data-target="#collapse_target">
-				<span class="navbar-toggler-icon"></span>
-			</button>
-			
-			<div class="collapse navbar-collapse" id="collapse_target">
-				<ul class="navbar-nav ml-auto">
-					<li class="nav-item">
-						<a class="nav-link" href="#">Home</a>
-					</li>
-					<li class="nav-item">
-						<a class="nav-link" href="#">Pages</a>
-					</li>
-					<li class="nav-item">
-						<a class="nav-link" href="#">Features</a>
-					</li>
-					<li class="nav-item">
-						<a class="nav-link" href="#">Portfolio</a>
-					</li>
-					<li class="nav-item">
-						<a class="nav-link" href="#">Blog</a>
-					</li>
-					<li class="nav-item">
-						<a class="nav-link" href="#">Contact</a>
-					</li>
+	<header>
+			<div id = "nav-bar">
+				<img class = "team-logo" src="#" alt = "team-logo"/>
+				<ul id="nav-bar-links">
+					<li><a href="#">Blog</a></li>
+					<li><a href="#">Events</a></li>
+					<li><a class="active" href="team-page.html">Teams</a></li>
+					<li><a href="#">About</a></li>
+					<li><a href="index.html">Home</a></li>
 				</ul>
-			</div>
-		</div>
-	</nav>
+			</div><!--End of nav-bar-->
+		</header>
+ 
 	<center>
 	<form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="GET">
-	<button type="submit" name="name"	    id="btnShow" value="Overwatch">Overwatch</button>
-	<button type="submit" name="name"		id="btnShow" value="Smash">Smash</button>
-	<button type="submit" name="name"		id="btnShow" value="Rocket League">Rocket League</button>
-	<button type="submit" name="name"		id="btnShow" value="CSGO">CSGO</button>
+	<?php include "php/buttons.php" ?>
 	</form>
 	</center>
+	
 	<div class="container-fluid">
+		<img src="images/<?php echo $search?>.jpg" alt="">
 		<div id="container" class="row">
-			<div id="col-a1" class="col-lg-2 text-center">
+			<div id="column" class="col">
 				
 			</div>
 			<div id="col-b" class="col-lg-2">
@@ -81,10 +74,6 @@
 			</div>
 		</div>
 	</div>
-	
-	<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
-	<script src="js/bootstrap.min.js"></script>
-	
  </body>
  </html>
  
--- team-page.php
+++ team-page.php
@@ -33,12 +33,13 @@
 		$result = $conn->query($sql);
 	}
 ?>
+
 <head>
 	<title>Indian Hills eSports - Team Page</title>
+	<link rel="stylesheet" type="text/css" href="css/team-style.css">
+	<!--link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"-->
 </head>
 <body>
-	<link rel="stylesheet" type="text/css" href="css/teamstyle.css">
-	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 	<div class="col-2">
 		<header>
 			<div id = "nav-bar">
@@ -47,44 +48,45 @@
 					<li><a href="#">Blog</a></li>
 					<li><a href="#">Events</a></li>
 					<li><a class="active" href="team-page.html">Teams</a></li>
-					<li><a href="#">About</a></li>
+					<li><a href="about-page.html">About</a></li>
 					<li><a href="index.html">Home</a></li>
 				</ul>
 			</div><!--End of nav-bar-->
 		</header>
-
+		
 		<br><br>
-
+		
+		<!-- Kelsey, coppy from here-->
 		<!-- Buttons -->
 		<div class = "game-list" style="text-align:center;">
 			<form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="GET">
-			<!-- This php loops and creates any number of buttons for the amount of games in the database -->
-			<?php include "php/buttons.php" ?>
+				<!-- This php loops and creates any number of buttons for the amount of games in the database -->
+				<?php include "php/buttons.php" ?>
 			</form>
 		</div><!-- End of Buttons -->
-
-		<main class="content">
-			<div class = "about">
-				<div class="card">
+				
+		<main class="content">	<!-- MAIN content -->		
+			<div class = "about"> <!-- DIV about -->	
 					<!-- This php loops to create the player cards for any number of players for a game. -->
 					  <?php include "php/playerCards.php" ?>
-				</div><!--End of Card-->
-				<br><br>
+				
+				<!-- to here for all the php ive worked on-->
+				
 				<h2>About the Team</h2>
 				<center>
-					<p>ESPORT – Lorem ipsum dolor sit amet, voluptua iracundia disputationi an pri, his utinam principes dignissim ad. Ne nec dolore oblique nusquam,
+					<p>ESPORT – Lorem ipsum dolor sit amet, voluptua iracundia disputationi an pri, his utinam principes dignissim ad. Ne nec dolore oblique nusquam, 
 					cu luptatum volutpat delicatissimi has. Sed ad dicam platonem, mea eros illum elitr id, ei has similique constituto. Ea movet saperet rationibus sit,
 					pri autem aliquip invidunt an. Consetetur omittantur consequuntur eos et. Eleifend praesent iudicabit no mea, tollit persequeris ex pri, tota splendide
 					voluptaria in pri. Ad per tale aliquip, ei sit.</p>
 					<h3>“Novum blandit adolescens sea te. Ea eum cetero scaevola.”</h3>
 					<p>In his meis porro viris, illud imperdiet reprimique et vim. Feugiat atomorum reprehendunt vix ei, ei facete regione pri. Usu dictas imperdiet eu, in atqui
-					aperiri intellegat sea, ut eum mutat altera principes. Te sit quaeque oportere, has modus inani ceteros ad. Impedit blandit deseruisse duo ea, ne graecis
-					deleniti incorrupte usu. Ut mei splendide accommodare. An pri iisque meliore. In his meis porro viris, illud imperdiet reprimique et vim. Feugiat atomorum
+					aperiri intellegat sea, ut eum mutat altera principes. Te sit quaeque oportere, has modus inani ceteros ad. Impedit blandit deseruisse duo ea, ne graecis 
+					deleniti incorrupte usu. Ut mei splendide accommodare. An pri iisque meliore. In his meis porro viris, illud imperdiet reprimique et vim. Feugiat atomorum 
 					reprehendunt vix ei pri.</p>
 				</center>
-			</div>
-		</main>
-
+				
+			</div> <!-- DIV about end -->
+		</main> <!-- MAIN content end -->
+		
 		<footer>Footer - eSports Links and Info</footer>
-	</div>
 </body>
